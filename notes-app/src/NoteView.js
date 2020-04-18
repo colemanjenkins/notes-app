ffb4dcfff4f338766ea37a9809a2a512499ac48b
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-04-17 22:16:47 -0400

    add menu bar

---
+++ notes-app/src/NoteView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./NoteView.css"
+
+class NoteView extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+export default NoteView;
