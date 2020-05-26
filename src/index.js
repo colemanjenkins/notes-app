230fec5a003c13bf841d99e399b8cc41c7e71bf0
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-05-26 10:45:29 -0400

    add firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const API_KEY = process.env.API_KEY;
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "notes-app-65619.firebaseapp.com",
+  databaseURL: "https://notes-app-65619.firebaseio.com",
+  projectId: "notes-app-65619",
+  storageBucket: "notes-app-65619.appspot.com",
+  messagingSenderId: "51778102347",
+  appId: "1:51778102347:web:201aa1ee0f60c3871519ce",
+  measurementId: "G-SPEXK6BEVP"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
